e6e18829333b5b311fb13dfec776ba8b4564e21e
Aleksandr <khamitov.aleksandr@gmail.com>
2023-01-12 09:44:50 +0100

    fix: add css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
-<header>
-    <img href="Logo.svg" alt="Logo of Little Lemon"/>
-    <nav>
-        <ul>
-          <li><a href='#home'>Home</a></li>
-          <li><a href='#about'>About</a></li>
-          <li><a href='#menu'>Menu</a></li>
-          <li><a href='#reservations'>Reservations</a></li>
-          <li><a href='#orderonline'>Order Online</a></li>
-          <li><a href='#login'>Login</a></li>
-        </ul>
-    </nav>
-</header>
+import "./styles.css";
+import React from 'react'
+
+function Header() {
+  return (
+      <header>
+          <img href="Logo.svg" alt="Logo of Little Lemon"/>
+          <nav>
+              <ul>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#menu'>Menu</a></li>
+                <li><a href='#reservations'>Reservations</a></li>
+                <li><a href='#orderonline'>Order Online</a></li>
+                <li><a href='#login'>Login</a></li>
+              </ul>
+          </nav>
+      </header>
+  )
+}
+
+export default Header;
