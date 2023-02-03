5a9c0122586711e785e90cd3e50d9f0572f96d14
khamitov <khamitov.aleksandr@gmail.com>
2023-02-03 16:51:24 +0100

    add: layout

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import Footer from './components/Footer';
 
-test("render a link of twitter", () => {
-  render(<Footer/>);
-  const linkTwitter = screen.getByAltText("Twitter");
-  expect(linkTwitter).toBeInTheDocument();
-});
+
+
+
